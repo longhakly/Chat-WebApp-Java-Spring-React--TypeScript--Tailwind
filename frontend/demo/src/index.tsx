58a4263ea14484458d533872d7d4e933af93bdf7
Long Hakly <long.hakly19@kit.edu.kh>
2023-06-08 22:23:31 +0700

    setup project for front end

---
+++ frontend/demo/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return <div className="bg-blue-500 text-white p-4">Hello, Tailwind CSS!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
